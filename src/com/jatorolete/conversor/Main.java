6cb500e79a46f9ca092e7312748c2e303d7cd15b
jatorolete <jatorolete@gmail.com>
2026-02-12 13:11:55 -0300

    estructura inicial del proyecto en Java

---
+++ src/com/jatorolete/conversor/Main.java
@@ -0,0 +1,8 @@
+package com.jatorolete.conversor;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al Conversor de Monedas");
+        
+    }
+}
